a633f8ce3cdf11ca0e9056c1a13a8e95ef6b7a86
elizabethmendez <elizabeth.zabala3@gmail.com>
2024-05-16 11:37:06 -0400

    task 0

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -2,5 +2,9 @@
 
 const fs = require('fs');
 fs.readFile(process.argv[2], 'utf8', function (error, content) {
-  console.log(error || content);
+  if (error) {
+    console.log(error);
+  } else {
+    console.log(content);
+  }
 });
