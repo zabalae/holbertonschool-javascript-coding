483aa1ef6e8c06f47882b8ac4454072ba0f7fe75
elizabethmendez <elizabeth.zabala3@gmail.com>
2024-05-16 11:34:35 -0400

    task 0

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,11 +1,6 @@
 #!/usr/bin/node
-// script that reads and prints the content of a file.
-
 
 const fs = require('fs');
 fs.readFile(process.argv[2], 'utf8', function (error, content) {
-  if (error) {
-    return console.log(error);
-  }
-  process.stdout.write(content);
+  console.log(error || content);
 });
