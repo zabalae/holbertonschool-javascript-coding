711eae94f1940c01c8c56350acf6965abdc18b61
elizabethmendez <elizabeth.zabala3@gmail.com>
2024-05-16 19:58:15 -0400

    task 5

---
+++ javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+const request = require('request');
+request(process.argv[2], function (error, response, body) {
+  if (error) {
+      console.log(error);
+  }
+  const completed = {};
+  for (const task of JSON.parse(body)) {
+    if (task.completed === true) {
+      if (completed[task.userId]) {
+        completed[task.userId]++;
+      } else {
+        completed[task.userId] = 1;
+      }
+    }
+  }
+  console.log(completed);
+});
