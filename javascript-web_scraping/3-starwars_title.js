f045e1307797c775fd42b0d6a8e74506ba629d5f
elizabethmendez <elizabeth.zabala3@gmail.com>
2024-05-16 19:04:19 -0400

    task 3

--- javascript-web_scraping/3-starwars_title.js
+++ javascript-web_scraping/3-starwars_title.js
@@ -1,8 +1,9 @@
 #!/usr/bin/node
 const request = require('request');
 request('https://swapi-api.hbtn.io/api/films/:id' + process.argv[2] + '/', function (error, response, body) {
-  if (error == null) {
-    const json = JSON.parse(body);
-    console.log(json.title);
+  if (error) {
+    console.log(error);
+  } else {
+    console.log(JSON.parse(body).title);
   }
-})
+});
