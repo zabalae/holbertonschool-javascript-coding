0928ef6a412694d6de9b79056d2c07ccad0d127f
elizabethmendez <elizabeth.zabala3@gmail.com>
2024-05-16 20:43:01 -0400

    task 8

--- javascript-web_scraping/101-starwars_characters.js
+++ javascript-web_scraping/101-starwars_characters.js
@@ -5,8 +5,12 @@ request(`https://swapi-api.hbtn.io/api/films/${process.argv[2]}`, async function
   for (const characterId of JSON.parse(body).characters) {
     await new Promise((resolve, reject) => {
       request(characterId, function (error, response, body) {
-       if (error) reject(error);
-       else resolve(JSON.parse(body).name);
+       if (error) {
+         reject(error);
+       } else {
+         console.log(JSON.parse(body).name);
+         resolve();
+       }
       });
     });
   }
