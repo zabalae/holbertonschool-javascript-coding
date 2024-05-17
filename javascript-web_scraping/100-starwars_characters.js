719e96640043f2072d88702aab0670d0dba59ee4
elizabethmendez <elizabeth.zabala3@gmail.com>
2024-05-16 20:17:31 -0400

    task 7

--- javascript-web_scraping/100-starwars_characters.js
+++ javascript-web_scraping/100-starwars_characters.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const request = require('request');
-request(`https://swapi-api.hbtn.io/api/films/${process.argv[2]}`, function(error, response, body) {
+request(`https://swapi-api.hbtn.io/api/films/${process.argv[2]}`, function (error, response, body) {
   if (error) console.log(error);
   for (const characterId of JSON.parse(body).characters) {
     request(characterId, function (error, response, body) {
