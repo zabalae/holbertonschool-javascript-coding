2f387435784a772d3532f58e09ce69c460ad5fc0
elizabethmendez <elizabeth.zabala3@gmail.com>
2024-05-16 20:21:27 -0400

    task 4

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -6,7 +6,7 @@ request.get(process.argv[2], function (error, response, body) {
     console.log(error);
   }
   for (let x = 0; JSON.parse(body).results[x] !== undefined; x++) {
-    if (JSON.parse(body).results[x].characters.includes(`https://swapi-api.hbtn.io/api/people/18/`)) {
+    if (JSON.parse(body).results[x].characters.includes('https://swapi-api.hbtn.io/api/people/18/')) {
       count++;
     }
   }
