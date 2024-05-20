370460ce5d1c35ece80a69c6c57e38d239c884ae
elizabethmendez <elizabeth.zabala3@gmail.com>
2024-05-20 18:53:47 -0400

    task 1

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,10 +1,10 @@
-process.stdout.write('Welcome to Holberton School, what is your name?\n');
+process.stdout.write("Welcome to Holberton School, what is your name?\n");
 
 process.stdin.on('readable', () => {
   const name = process.stdin.read();
 
-  if (name.length > 0) {
-    process.stdin.write(`Your name is: ${name}`);
+  if (name) {
+    process.stdout.write(`Your name is: ${name}`);
   }
 });
 
