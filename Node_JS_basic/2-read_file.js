cb108a4cc76735c0fcfb5da132c8270c318b4f47
Elizabeth Mendez <40727857+zabalae@users.noreply.github.com>
2024-05-21 14:26:33 -0400

    Update 2-read_file.js

--- Node_JS_basic/2-read_file.js
+++ Node_JS_basic/2-read_file.js
@@ -6,20 +6,20 @@ function countStudents(path) {
   }
   const data = fs.readFileSync(path, 'utf8');
   const students = data.split('\n')
-      .map((student) => student.split(','))
-      .filter((student) => student.length === 4 && student[0] !== 'firstname')
-      .map((student) => ({
-        firstName: student[0],
-        lastName: student[1],
-        age: student[2],
-        field: student[3],
-      }));
+    .map((student) => student.split(','))
+    .filter((student) => student.length === 4 && student[0] !== 'firstname')
+    .map((student) => ({
+      firstName: student[0],
+      lastName: student[1],
+      age: student[2],
+      field: student[3],
+    }));
   const csStudents = students
-      .filter((student) => student.field === 'CS')
-      .map((student) => student.firstName);
+    .filter((student) => student.field === 'CS')
+    .map((student) => student.firstName);
   const sweStudents = students
-      .filter((student) => student.field === 'SWE')
-      .map((student) => student.firstName);
+    .filter((student) => student.field === 'SWE')
+    .map((student) => student.firstName);
   console.log(`Number of students: ${students.length}`);
   console.log(`Number of students in CS: ${csStudents.length}. List: ${csStudents.join(', ')}`);
   console.log(`Number of students in SWE: ${sweStudents.length}. List: ${sweStudents.join(', ')}`);
