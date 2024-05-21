b9dfac67d86d9421fdc32f56fc83b882d4610aa6
elizabethmendez <elizabeth.zabala3@gmail.com>
2024-05-21 18:09:29 -0400

    task 7

--- Node_JS_basic/7-http_express.js
+++ Node_JS_basic/7-http_express.js
@@ -9,17 +9,18 @@ app.get('/', (req, res) => {
   res.setHeader('Content-Type', 'text/plain');
   res.send('Hello Holberton School!');
 });
-  app.get('/students', async (req, res) => {
-    res.write('This is the list of our students\n');
-    res.setHeader('Content-Type', 'text/plain');
-    res.write('This is the list of our students\n');
-    await students(process.argv[2]).then((data) => {
-      res.write(`Number of students: ${data.students.length}\n`);
-      res.write(`Number of students in CS: ${data.csStudents.length}. List: ${data.csStudents.join(', ')}\n`);
-      res.write(`Number of students in SWE: ${data.sweStudents.length}. List: ${data.sweStudents.join(', ')}`);
-      res.end();
-    }).catch((err) => res.end(err.message))
-        .finally(() => {
+
+app.get('/students', async (req, res) => {
+  res.write('This is the list of our students\n');
+  res.setHeader('Content-Type', 'text/plain');
+  res.write('This is the list of our students\n');
+  await students(process.argv[2]).then((data) => {
+    res.write(`Number of students: ${data.students.length}\n`);
+    res.write(`Number of students in CS: ${data.csStudents.length}. List: ${data.csStudents.join(', ')}\n`);
+    res.write(`Number of students in SWE: ${data.sweStudents.length}. List: ${data.sweStudents.join(', ')}`);
+    res.end();
+  }).catch((err) => res.end(err.message))
+      .finally(() => {
           res.end();
         });
   });
