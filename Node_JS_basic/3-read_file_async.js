4aa03483d4e0022788456f6a72ac31047b2d83e9
elizabethmendez <elizabeth.zabala3@gmail.com>
2024-05-21 15:37:39 -0400

    task 3

--- Node_JS_basic/3-read_file_async.js
+++ Node_JS_basic/3-read_file_async.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 
 const countStudents = (path) => new Promise((resolve, reject) => {
-  fs.readFile(path, 'utf8', (err, data) => {
+  fs.readFile(path, 'utf-8', (err, data) => {
     if (err) {
       reject(new Error('Cannot load the database'));
     }
