9d923df7155baeb72ab7547a6bfcbf86811950c8
Elizabeth Mendez <40727857+zabalae@users.noreply.github.com>
2024-05-31 17:31:33 -0400

    Update AppController.js

--- Node_JS_basic/full_server/controllers/AppController.js
+++ Node_JS_basic/full_server/controllers/AppController.js
@@ -1,8 +1,7 @@
 class AppController {
-  static getHome(req, res) {
-    res.status(200).send('Hello Holberton School!')
+  static getHomepage(request, response) {
+    response.send(200, 'Hello Holberton School!');
   }
 }
-
+  
 export default AppController;
-module.exports = AppController;
